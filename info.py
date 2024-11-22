223412693706bb7487d275bc2efabb55ceb12f4e
abdullah74400 <146393881+abdullah74400@users.noreply.github.com>
2024-11-22 22:21:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '27498647'))
-API_HASH = environ.get('API_HASH', 'f7764b70861584bca951040781111bfb')
+API_ID = int(environ.get('API_ID', '24111429'))
+API_HASH = environ.get('API_HASH', '41c451e7412d4225f0a5450a166bcf7a')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
@@ -26,12 +26,12 @@ NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/0a3dd8abd3f74b7f7f365.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/0e2bc4c69e7a471e242b8.jpg'))
-CODE = (environ.get('CODE', 'https://t.me/ak_backup_72/8'))
+CODE = (environ.get('CODE', 'https://t.me/ydv2243'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'shortxlinks.com'))
-STREAM_API = (environ.get('STREAM_API', '12e70a293a288f959126f3652544be0de3aac3e8'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/ak_backup_72/6'))
+STREAM_API = (environ.get('STREAM_API', '8a845588bcc9ce0e202f90c9db9b7bedb58f9b6b'))
+STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/ydv2243'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1807927334').split()]
@@ -39,7 +39,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '1807927334').split()]
-auth_channel = environ.get('AUTH_CHANNEL', '-1002000099438')
+auth_channel = environ.get('AUTH_CHANNEL', '')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -52,25 +52,25 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://IronMan72:khan7860@cluster0.q566bxw.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Tony:khan7860@cluster0.6wfpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "IronMan72")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Rolex72")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '6a3fd84018381fe30f7e1cb2e9a41f92abfb2dd8')
+SHORTLINK_API = environ.get('SHORTLINK_API', '8a845588bcc9ce0e202f90c9db9b7bedb58f9b6b')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/ak_bot_support')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/ak_backup_72')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/ak_backup_72/6')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Search_movies_name')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/ydv2243')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/ydv2243')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : Ak74400')
+MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : Vivek_1020')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001933029304'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ak_bot_support')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Search_movies_name')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
